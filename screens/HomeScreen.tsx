ef022e4d4e8e0cef29a01c4496d71130b527dd77
obakengshole <shole.obakeng@gmail.com>
2021-11-23 14:38:40 +0200

    Added improvements to VideoCard view on HomeScreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Image, Text,StyleSheet } from 'react-native'
+import { Entypo } from '@expo/vector-icons';
 
 const HomeScreen = () => {
     return (
@@ -13,7 +14,21 @@ const HomeScreen = () => {
                         <Text style={ styles.time }>15:23</Text>
                     </View>
                 </View>
-                {/* Title row */} 
+                
+                {/* Title row */}
+                <View style={ styles.titleRow }>
+                    {/* Avatar */}
+                    <Image style={ styles.avatar } source={{ uri: 'https://trendsinusa.com/wp-content/uploads/2018/01/Anonymous-hacker-profile-picture.jpg' }}/>
+
+
+                    {/* Middle container: Title, subtitle, etc */}
+                    <View style={ styles.middleContainer }>
+                        <Text style={ styles.title }>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</Text>
+                        <Text style={ styles.subtitle }>Obakeng  1.5m  2 months ago</Text>
+                    </View>
+                    {/* Icon */}
+                    <Entypo name="dots-three-vertical" size={16} color="white" />
+                </View>
             </View>
         </View>
     )
@@ -39,7 +54,33 @@ const styles = StyleSheet.create({
     time: {
         color: 'white',
         fontWeight: 'bold'
+    },
+    avatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+    titleRow: {
+        flexDirection: 'row',
+        padding: 10,
+        borderColor: 'red',
+        borderWidth: 5
+    },
+    middleContainer: {
+        marginHorizontal: 10,
+        flex: 1
+    },
+    title: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: "500",
+        marginBottom: 5
+    },
+    subtitle: {
+        color: 'grey',
+        fontSize: 14,
+        fontWeight: "500"
     }
-})
+    })
 
 export default HomeScreen
