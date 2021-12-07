ee1044227a272458df7108feca0cb097ef3e52a6
obakengshole <shole.obakeng@gmail.com>
2021-12-07 16:26:54 +0200

    Added BottomSheet to VideoScreen, but had to comment it out it since I couldnt get it to work

--- screens/VideoScreen/VideoScreen.tsx
+++ screens/VideoScreen/VideoScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   SafeAreaView,
   Text,
@@ -6,15 +6,22 @@ import {
   View,
   ScrollView,
   FlatList,
+  Pressable
 } from "react-native";
+// import BottomSheet from "@gorhom/bottom-sheet";
 import styles from "./styles";
 import VideoListItem from "../../components/VideoListItem";
+import VideoPlayer from "../../components/VideoPlayer";
 
 import video from "../../assets/data/video.json";
 import videos from "../../assets/data/videos.json";
 import { AntDesign } from "@expo/vector-icons";
 
 const VideoScreen = () => {
+
+  // const commentsSheetRef = useRef<BottomSheet(null)
+  const commentsSheetRef = useRef(null)
+
   let viewsString = video.views.toString();
   if (video.views > 1000000) {
     viewsString = (video.views / 1000000).toFixed(1) + "m";
@@ -22,89 +29,103 @@ const VideoScreen = () => {
     viewsString = (video.views / 1000).toFixed(1) + "k";
   }
 
+  const openComments = () => {
+    commentsSheetRef.current?.expand()
+  }
+
   return (
     <SafeAreaView style={{ backgroundColor: "#141414", flex: 1 }}>
       {/* Video Player */}
-      <Image style={styles.videoPlayer} source={{ uri: video.thumbnail }} />
+      <VideoPlayer videoURI={video.videoUrl} thumbnailURI={video.thumbnail} />
 
-      {/* Video Info */}
-      <View style={styles.videoInfoContainer}>
-        <Text style={styles.tags}>{video.tags}</Text>
-        <Text style={styles.title}>{video.title}</Text>
-        <Text style={styles.subtitle}>
-          {video.user.name} {viewsString} views {video.createdAt}
-        </Text>
-      </View>
+      <View style={{ flex: 1 }}>
+        {/* <Image style={styles.videoPlayer} source={{ uri: video.thumbnail }} /> */}
 
-      {/* Action list */}
-      <View style={styles.actionListContainer}>
-        <ScrollView horizontal showHorizontalScrollIndicator={false}>
-          <View style={styles.actionListItem}>
-            <AntDesign name="like1" size={30} color="lightgrey" />
-            <Text style={styles.actionText}>{video.likes}</Text>
-          </View>
-
-          <View style={styles.actionListItem}>
-            <AntDesign name="dislike2" size={30} color="lightgrey" />
-            <Text style={styles.actionText}>{video.dislikes}</Text>
-          </View>
-
-          <View style={styles.actionListItem}>
-            <AntDesign name="export" size={30} color="lightgrey" />
-            <Text style={styles.actionText}>{video.dislikes}</Text>
-          </View>
-
-          <View style={styles.actionListItem}>
-            <AntDesign name="download" size={30} color="lightgrey" />
-            <Text style={styles.actionText}>{video.dislikes}</Text>
-          </View>
-        </ScrollView>
-      </View>
-
-      {/* User Info */}
-      <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
-        <Image style={styles.avatar} source={{ uri: video.user.image }} />
-
-        <View style={{ marginHorizontal: 10, flex: 1 }}>
-          <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>
-            {video.user.name}
-          </Text>
-          <Text style={{ color: "grey", fontSize: 18 }}>
-            {video.user.subscribers}
+        {/* Video Info */}
+        <View style={styles.videoInfoContainer}>
+          <Text style={styles.tags}>{video.tags}</Text>
+          <Text style={styles.title}>{video.title}</Text>
+          <Text style={styles.subtitle}>
+            {video.user.name} {viewsString} views {video.createdAt}
           </Text>
         </View>
 
-        <Text
-          style={{
-            color: "red",
-            fontSize: 18,
-            fontWeight: "bold",
-            padding: 10,
-          }}
-        >
-          Subscribe
-        </Text>
-      </View>
+        {/* Action list */}
+        <View style={styles.actionListContainer}>
+          <ScrollView horizontal showHorizontalScrollIndicator={false}>
+            <View style={styles.actionListItem}>
+              <AntDesign name="like1" size={30} color="lightgrey" />
+              <Text style={styles.actionText}>{video.likes}</Text>
+            </View>
 
-      {/* Comments */}
-      <View style={{ padding: 10, marginVertical: 10 }}>
-        <Text style={{ color: "white" }}>Comments 333</Text>
+            <View style={styles.actionListItem}>
+              <AntDesign name="dislike2" size={30} color="lightgrey" />
+              <Text style={styles.actionText}>{video.dislikes}</Text>
+            </View>
+
+            <View style={styles.actionListItem}>
+              <AntDesign name="export" size={30} color="lightgrey" />
+              <Text style={styles.actionText}>{video.dislikes}</Text>
+            </View>
+
+            <View style={styles.actionListItem}>
+              <AntDesign name="download" size={30} color="lightgrey" />
+              <Text style={styles.actionText}>{video.dislikes}</Text>
+            </View>
+          </ScrollView>
+        </View>
+
+        {/* User Info */}
         <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            marginVertical: 10,
-          }}
+          style={{ flexDirection: "row", alignItems: "center", padding: 10 }}
         >
-          <Image
-            style={{ width: 35, height: 35, borderRadius: 20 }}
-            source={{ uri: video.user.image }}
-          />
-          <Text style={{ color: "white", marginLeft: 10 }}>
-            Lorem asdsf asdfasdfd as fasdf afawe herthrthr rhrthr Lorem asdsf
-            asdfasdfd as fasdf afawe herthrthr rhrthr
+          <Image style={styles.avatar} source={{ uri: video.user.image }} />
+
+          <View style={{ marginHorizontal: 10, flex: 1 }}>
+            <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>
+              {video.user.name}
+            </Text>
+            <Text style={{ color: "grey", fontSize: 18 }}>
+              {video.user.subscribers}
+            </Text>
+          </View>
+
+          <Text
+            style={{
+              color: "red",
+              fontSize: 18,
+              fontWeight: "bold",
+              padding: 10,
+            }}
+          >
+            Subscribe
           </Text>
         </View>
+
+        {/* Comments */}
+        <Pressable onPress={openComments} style={{ padding: 10, marginVertical: 10 }}>
+          <Text style={{ color: "white" }}>Comments 333</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 10,
+            }}
+          >
+            <Image
+              style={{ width: 35, height: 35, borderRadius: 20 }}
+              source={{ uri: video.user.image }}
+            />
+            <Text style={{ color: "white", marginLeft: 10 }}>
+              Lorem asdsf asdfasdfd as fasdf afawe herthrthr rhrthr Lorem asdsf
+              asdfasdfd as fasdf afawe herthrthr rhrthr
+            </Text>
+          </View>
+          </Pressable>
+          {/* 
+      <BottomSheet ref={commentsSheetRef} snapPoints={[0, '100%']} index={-1} enablePanDownToClose={false}>
+        <Text>Hello</Text>
+      </BottomSheet> */}
       </View>
 
       {/* Recommended videos */}
@@ -121,7 +142,7 @@ const VideoScreenWithRecommendations = () => {
         ListHeaderComponent={VideoScreen}
       />
     </SafeAreaView>
-  )
+  );
 };
 
-export default VideoScreenWithRecommendations
+export default VideoScreenWithRecommendations;
